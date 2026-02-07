bf129cab7b328981b8f77f12e79dbd18442d1d99
Dan <danielgao2766@gmail.com>
2026-02-07 18:06:38 -0500

    restored file

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+
 
 public class UserInterface {
+    UserInterface(String[] args){
+        inputLogic(args);
+    }
+    public void inputLogic(String[] args){
+        if(args == null){System.out.print("passed");}
+        else{System.out.print("failed");}
+    }
 }
