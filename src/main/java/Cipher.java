2fee06b3f51e9db46127d8286f7c8d42691271b0
Shivaths Karne <karneshivaths@gmail.com>
2026-02-12 13:20:03 -0500

    cipher.java Finished

---
+++ src/main/java/Cipher.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+public class Cipher {
+    private static Map<Character, Character> decodingMap = null;
+    private static final String KEY_FILE = "ciphers/key.txt";
+
+    public static String decrypt(String input) {
+        String keyContent = FileHandler.readFile("ciphers/key.txt");
+        if (keyContent == null || keyContent.isEmpty()) {
+            return input; // Return input as is if key file is missing or empty
+        }
+        String[] lines = keyContent.split("\n");
+        if (lines.length < 2) {
+            return input;
+        }
+        String decodedChars = lines[0].trim();
+        String  encodedChars= lines[1].trim();
+        String result = "";
+        for (char currentLetter : input.toCharArray()) {
+
+            // Find where this letter exists in the 'cipherRow'
+            int index = decodedChars.indexOf(currentLetter);
+
+            // If we found the letter in the cipher row (index is not -1)
+            if (index != -1) {
+                // Pick the corresponding letter from the 'plainRow'
+                char plainLetter = encodedChars.charAt(index);
+                result += plainLetter;
+            } else {
+                // If the letter isn't in the key (like a space or punctuation), keep it as is
+                result += currentLetter;
+            }
+        }
+
+        return result.toString();
+
+
+    }
+}
